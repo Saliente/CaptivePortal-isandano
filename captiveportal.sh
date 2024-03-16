63f145e6aae1b7b5235ccbc31d0b24487ddff83d
Pedro Felipe <18344255+Saliente@users.noreply.github.com>
2024-03-16 14:29:30 -0300

    Update captiveportal.sh

--- captiveportal.sh
+++ captiveportal.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-# Remove old docker files 
+#!/bin/bash                                                                                                                                                                                                                                                                                                       #remove old docker files                                                                              sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
 
-sudo su
-sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-# install repo
+#install new repo
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
-# install docker
+
+#install docker
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# start and enable startup docker
+
+#start and enable docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
@@ -35,13 +34,17 @@ StartLimitBurst=3
 [Install]
 WantedBy=multi-user.target
 EOF
-# enable startup 
+
+#enable startup
+sudo systemctl enable captiveportal.service
+
+# enable startup
 sudo systemctl enable captiveportal.service
 
 # create a folder to save the file
 sudo mkdir /captive_portal
-
-# create docker compose file 
+sudo chown $USER:$USER -R /captive_portal
+# create docker compose file
 sudo cat << 'EOF'>> /captive_portal/docker-compose.yaml
 version: "3"
 services:
@@ -53,5 +56,6 @@ services:
     command: /bin/bash -c "service apache2 start && service mysql start && tail -f /var/log/mysql/error.log /var/log/apache2/error.log "
 EOF
 
-# start docker compose 
+# start docker compose
 sudo docker compose -f /captive_portal/docker-compose.yaml up -d
+
